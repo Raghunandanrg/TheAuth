ff026e09ae2fb8c05d9c80f348424a245f2e770e
Raghunandanrg <raghunandanrg1999@gmail.com>
2024-05-16 13:03:00 +0530

    my first route, controller

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test=(req,res)=>{
+    res.json({
+        message:"Congralutions your api is working"
+    })
+}
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import UserRoutes from './routes/user.route.js'
 dotenv.config()
 const app=express()
 
@@ -14,3 +15,5 @@ Mongoose.connect(process.env.MONGO).then(()=>[
 app.listen(3000,()=>{
     console.log("server running on port 3000")
 })
+
+app.use('/api/user/',UserRoutes)
