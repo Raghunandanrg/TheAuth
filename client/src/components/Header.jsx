201e4f43eb241193a77585fc8de967723f414b68
Raghunandanrg <raghunandanrg1999@gmail.com>
2024-07-06 18:17:26 +0530

    updated header and profile page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,8 +4,6 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
     const { currentUser } = useSelector((state) => state.user)
-    console.log(currentUser)
-
     return (
         <div className='bg-slate-200'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3 '>
